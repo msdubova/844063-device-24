742d57645cad24012a1ca2374e065f7c2bf8ae72
msdubova <femme777@gmail.com>
2019-03-04 19:24:34 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,110 @@
-var link  = document.querySelector(".feedback");
-var map = document.querySelector(".mapping");
+
+
+// var openform = document.querySelector(".feedback");
+// var popup = document.querySelector(".write-us");
+// var closeform = form.querySelector(".close");
+// var name = popup.querySelector("[name=name-surname]");
+// var mail = popup.querySelector("[name=e-mail]");
+// var text = popup.querySelector("textarea");
+// var form = popup.querySelector("form");
+// var isStorageSupport = true;
+// var storage = "";
+// var openmap = document.querySelector(".mapping");
+// var map = document.querySelector(".map");
+// var closemap = map.querySelector(".close");
+
+// try {
+// storage = localStorage.getItem("name");
+// } catch (err){
+// isStorageSupport = false;
+// }
+
+
+// form.addEventListener ("submit", function(evt){
+// if(!name.value || !mail.value || !text.value){
+// evt.preventDefault();
+// console.log("Введите имя почту и сообщение");
+// }
+// else {
+// if(isStorageSupport){
+// localStorage.setItem("name",name.value);
+// localStorage.setItem("mail",mail.value);
+// localStorage.setItem("text",text.value);
+// }
+// }
+
+// });
+
+
+var openForm = document.querySelector(".feedback");
 var popup = document.querySelector(".write-us");
-var mapbig = document.querySelector(".map");
-var close = popup.querySelector(".close");
-var closemap = mapbig.querySelector(".close");
-var login = popup.querySelector("[name=name-surname]");
-link.addEventListener("click", function(evt){
+var closeForm  = popup.querySelector(".close");
+var openMap = document.querySelector(".mapping");
+var map = document.querySelector(".map");
+var closeMap = map.querySelector(".close");
+var form  = popup.querySelector("form");
+var storage = "";
+var isStorageSupport = true;
+var first  = popup.querySelector("[name=name-surname]");
+var second = popup.querySelector("[name=e-mail]");
+var third = popup.querySelector("textarea");
+
+try {
+  storage=localStorage.getItem("name");
+}
+catch (err) {
+  isStorageSupport = false;
+}
+
+openForm.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.add("modal-show");
-  login.focus();
+  if(storage){
+    localStorage.getItem("first");
+    second.focus();
+  }
+  else {
+    first.focus();
+  }
+
 });
-close.addEventListener("click", function(evt){
+
+closeForm.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
-map.addEventListener("click", function(evt){
+
+openMap.addEventListener("click", function(evt){
   evt.preventDefault();
-  mapbig.classList.add("modal-show");
+  map.classList.add("modal-show");
 });
-closemap.addEventListener("click", function(evt){
+
+closeMap.addEventListener("click", function(evt){
   evt.preventDefault();
-  mapbig.classList.remove("modal-show");
+  map.classList.remove("modal-show");
+});
+
+ form.addEventListener("submit", function(evt){
+   if (!first.value || !second.value || !third.value) {
+  evt.preventDefault();
+  console.log("введите данные")
+} else {
+  if(isStorageSupport){
+    localStorage.setItem("first", first.value);
+    localStorage.setItem("second", second.value);
+    localStorage.setItem("third", third.value);};}
+
+});
+
+window.addEventListener("keydown", function(evt) {
+  if(evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")){
+      popup.classList.remove("modal-show")
+    }
+    else { if (map.classList.contains("modal-show")) {
+      map.classList.remove("modal-show")
+    }
+  }
+  }
 });
